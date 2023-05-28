6874cea05d43ba58ef8a7a21ca254adb5458ba95
Roshan Paudel <rozanpoudel@gmail.com>
2023-05-28 14:15:41 +0545

    made button toggle

--- src/switch/Switch.jsx
+++ src/switch/Switch.jsx
@@ -1,11 +1,22 @@
 import './switch.css';
 import { useTheme } from '../ThemeContext';
+import { useState } from 'react';
 
 const Switch = () => {
+  const [dtheme, setdTheme] = useState('light');
+
+  function handleClick() {
+    if (dtheme == 'light') {
+      setdTheme('dark');
+    } else {
+      setdTheme('light');
+    }
+  }
+
   const { theme } = useTheme();
   return (
     <label className='switch'>
-      <input type='checkbox' checked={theme === 'light'} />
+      <input type='checkbox' checked={theme === dtheme} onClick={handleClick} />
       <span className='slider round' />
     </label>
   );
