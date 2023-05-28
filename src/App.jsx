99a49ed580838f75365d03d4622e994509a7ae04
Roshan Paudel <rozanpoudel@gmail.com>
2023-05-28 13:41:41 +0545

    bootstrapped

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-
 const App = () => {
-  const [radius, setRadius] = useState(0);
-  function handleChange(e) {
-    setRadius(e.target.value);
-  }
-
-  const mystyle = {
-    border: `${radius}px solid black`,
-    backgroundColor: 'red',
-    height: '200px',
-    width: '200px',
-    borderRadius: `${radius}%`,
-  };
   return (
-    <div className='circle'>
-      <div style={mystyle}></div>
-
-      <h1>Radius : {radius}%</h1>
-      <input
-        style={{ width: '50vw' }}
-        type='range'
-        min={0}
-        max={50}
-        value={radius}
-        onChange={handleChange}
-      />
-    </div>
+    <>
+      <h1>Hello world</h1>
+      <div></div>
+    </>
   );
 };
 
