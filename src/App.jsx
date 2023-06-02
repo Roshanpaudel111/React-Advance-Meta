c5c1f113e800c1416525956193b9b4e2da2ec17e
Roshan Paudel <rozanpoudel@gmail.com>
2023-06-02 06:45:52 +0545

    data fetching done !!

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+const api = 'https://randomuser.me/api/?results=1';
 
 function App() {
   const [user, setUser] = React.useState([]);
 
-  const fetchData = () => {};
+  const fetchData = () => {
+    fetch(api)
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error('Error' + res.status);
+        }
+      })
+      .then((data) => {
+        setUser(data);
+        console.log(user);
+      });
+  };
 
   React.useEffect(() => {
     fetchData();
@@ -12,6 +26,8 @@ function App() {
   return Object.keys(user).length > 0 ? (
     <div style={{ padding: '40px' }}>
       <h1>Customer data</h1>
+      <h3>Name : {user.results[0].id.name}</h3>
+      <img src={user.results[0].picture.large} alt='' />
     </div>
   ) : (
     <h1>Data pending...</h1>
